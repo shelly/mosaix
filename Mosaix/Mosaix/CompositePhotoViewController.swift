e9c665fdfaf52e42248d8dca57fe13a85b6cdd05
Shelly Bensal <shelly.bensal@gmail.com>
2017-04-15 18:34:48 -0400

    Functioning view controllers, segues between them

--- Mosaix/Mosaix/CompositePhotoViewController.swift
+++ Mosaix/Mosaix/CompositePhotoViewController.swift
@@ -1,6 +1,4 @@
 //
-//  OptionsViewController.swift
-//  Mosaix
 //
 //  Created by Nathan Eliason on 4/11/17.
 //  Copyright © 2017 Nathan Eliason. All rights reserved.
@@ -8,17 +6,18 @@
 
 import UIKit
 
-class OptionsViewController: UIViewController {
-
+class CompositePhotoViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }   
+    }
+    
 
 }
---
+++ Mosaix/Mosaix/CreateMosiacViewController.swift
@@ -0,0 +1,44 @@
+//
+//
+//  Created by Nathan Eliason on 4/11/17.
+//  Copyright © 2017 Nathan Eliason. All rights reserved.
+//
+
+import UIKit
+
+class CreateMosiacViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Transitions to other screens
+    
+    //Forget/save any settings
+    @IBAction func cancelCreateMosiacView() {
+        
+    }
+    
+    //Save any settings
+    @IBAction func goToOptions() {
+        
+    }
+    
+    //Launch composite photo creation
+    @IBAction func createCompositePhoto() {
+        
+    }
+
+    //Restarting createMosaic screen 
+    @IBAction func backToCreateMosaic(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    
+    
+}
