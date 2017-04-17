c90cf491b88871660e400bc897bdc7274057b032
Shelly Bensal <shelly.bensal@gmail.com>
2017-04-17 03:27:41 -0400

    Functioning sliders

--- Mosaix/Mosaix/CreateMosaicViewController.swift
+++ Mosaix/Mosaix/CreateMosaicViewController.swift
@@ -12,12 +12,33 @@ class CreateMosaicViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     var image: UIImage!
     var mosaicCreator: MosaicCreator!
+    @IBOutlet weak var qualitySlider: UISlider! = UISlider()
+    @IBOutlet weak var sizeSlider: UISlider! = UISlider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
         // Do any additional setup after loading the view.
         mosaicCreator = MosaicCreator(reference: image)
+        
+        qualitySlider.minimumValue = Float(MosaicCreationConstants.qualityMin)
+        qualitySlider.maximumValue = Float(MosaicCreationConstants.qualityMax)
+        sizeSlider.minimumValue = Float(MosaicCreationConstants.gridSizeMin)
+        sizeSlider.maximumValue = Float(MosaicCreationConstants.gridSizeMax)
+        
+        let qualitySliderDefault = Float(MosaicCreationConstants.qualityMax - MosaicCreationConstants.qualityMin)/2
+        let sizeSliderDefault = Float(MosaicCreationConstants.gridSizeMax - MosaicCreationConstants.gridSizeMin)/2
+        
+        qualitySlider.value = qualitySliderDefault
+        sizeSlider.value = sizeSliderDefault
+        
+        do {
+            try mosaicCreator.setQuality(quality: Int(qualitySliderDefault))
+            try mosaicCreator.setGridSizePoints(gridSizePoints: Int(sizeSliderDefault))
+        }
+        catch {
+            print("Issue with initial setting of setting quality/grid size points.\n")
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -37,6 +58,29 @@ class CreateMosaicViewController: UIViewController {
         
     }
     
+    @IBAction func qualityChanged(sender: UISlider){
+        let value = Int(sender.value)
+        do {
+            try mosaicCreator.setQuality(quality: value)
+        }
+        catch {
+            print("Error with setting quality.\n")
+        }
+        
+        
+    }
+
+    @IBAction func sizeChanged(sender: UISlider){
+        let value = Int(sender.value)
+        do {
+            try mosaicCreator.setGridSizePoints(gridSizePoints: value)
+        }
+        catch {
+            print("Error with setting grid size.\n")
+        }
+        
+    }
+    
     //Launch composite photo creation
     @IBAction func createCompositePhoto() {
         print("Creating composite photo!!")
@@ -44,7 +88,7 @@ class CreateMosaicViewController: UIViewController {
             try mosaicCreator.begin()
         }
         catch {
-            print("We broke it!!")
+            print("Error with calling mosaicCreator.begin.\n")
         }
     }
 
